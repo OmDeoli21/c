0f2748c00a99f7bf6ae0a1c69c1fd953e2dfc474
OmDeoli21 <omdeoli06@gmail.com>
2025-07-31 21:57:43 +0530

    c program to find a leap year

--- PS3second.c
+++ PS3second.c
@@ -1,3 +1,5 @@
+//codewithharry practice set 3 q2
+
 // Calculate income tax paid by an employee to the government as per the slabs
 // mentioned below:
 //  Income Slab Tax
@@ -7,26 +9,50 @@
 // Note that there is no tax below 2.5L. Take income amount as an input from the user
 
 
+// #include <stdio.h>
+// int main(){
+//     float income;
+//     printf("Type your income:  \n");
+//     scanf("%f", &income);
+
+//     if(income<250000){
+//         printf("NO TAX!!");
+//     }
+//     if(income>250000 && income<500000){
+//         printf("Tax to be paid by you : %.1f", income/20);
+//     }
+//      if(income>500000 && income<1000000){
+//         printf("Tax to be paid by you : %.1f", income/5);
+//     }
+//     else
+//     {
+//         printf("tax to be paid by you : %.1f", income*0.3);
+//     }
+
+//     return 0;
+// }
+
+
+//codewithharry problemset3 q4
+
+
 #include <stdio.h>
 int main(){
-    float income;
-    printf("Type your income:  \n");
-    scanf("%f", &income);
+    printf("enter a year: \n");
+    int year;
+    scanf("%d", &year);
 
-    if(income<250000){
-        printf("NO TAX!!");
+    if((year%4==0 && year%100 !=0) || year%400==0  ){
+        printf("this is a leap year !");
+
     }
-    if(income>250000 && income<500000){
-        printf("Tax to be paid by you : %.1f", income/20);
-    }
-     if(income>500000 && income<1000000){
-        printf("Tax to be paid by you : %.1f", income/5);
-    }
-    else
-    {
-        printf("tax to be paid by you : %.1f", income*0.3);
+    else{
+        printf("not a leap year");
     }
 
     return 0;
 }
-//codewithharry practice set 3 q2
+
+
+
+
