269209a94d6407c74ed576b8d399c577fa6d9b72
OmDeoli21 <omdeoli06@gmail.com>
2025-08-03 19:48:00 +0530

    program to find product of digits of integer

--- sixth6.c
+++ sixth6.c
@@ -1,16 +1,36 @@
-/*write a program to print the input integer in reverse*/
+// /*write a program to print the input integer in reverse*/
+// #include<stdio.h>
+// int main(){
+//     int a , b ;
+//     printf("enter a integer to be printed in reverse : ");
+//     scanf("%d",&a);
+//     while(a!=0){
+//         b = a%10;
+//         a = a/10;
+//          printf("%d",b);
+        
+//     }
+
+
+//     return 0;
+// }
+
+
+
+/*write a program to find product of digits of integer*/
+
 #include<stdio.h>
 int main(){
-    int a , b ;
-    printf("enter a integer to be printed in reverse : ");
+    int a,b,multi=1;
+    printf("enter a number to know product of its integer \n");
     scanf("%d",&a);
-    while(a!=0){
+    while(a!=0 ){
         b = a%10;
         a = a/10;
-         printf("%d",b);
+        multi = multi * b ;
         
     }
-
+    printf("%d",multi);
 
     return 0;
 }
