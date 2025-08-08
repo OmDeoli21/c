57138370946f61b0b3f88a3112ff3759e76dda7d
OmDeoli21 <omdeoli06@gmail.com>
2025-08-08 23:35:08 +0530

    hello

--- PS1first.c
+++ PS1first.c
@@ -326,19 +326,32 @@
 
 /*[q]Print number from number to 0*/
 
-#include <stdio.h>
-int main(){
-    printf("enter a number from 1-10 \t");
-    int a;
-    scanf("%d", &a);
-    while(a>=0){
-        printf("%d \n",a);
-        a = a-1;
+// #include <stdio.h>
+// int main(){
+//     printf("enter a number from 1-10 \t");
+//     int a;
+//     scanf("%d", &a);
+//     while(a>=0){
+//         printf("%d \n",a);
+//         a = a-1;
 
-    }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ #include<stdio.h>
+ int main(){
+    printf("hello");
+    return 0;
+ }
+
+
+
+
+
+
+
