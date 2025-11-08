a225598d08d78b58669d2b86a8176313b1e76799
OmDeoli21 <omdeoli06@gmail.com>
2025-11-08 18:37:33 +0530

    Write a program to calculate the sum of the numbers occurring in the multiplication table of 8. (consider 8 x 1 to 8 x 10)

--- PS1first.c
+++ PS1first.c
@@ -510,3 +510,18 @@
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+/*Write a program to calculate the sum of the numbers occurring in the
+multiplication table of 8. (consider 8 x 1 to 8 x 10)*/
+
+#include<stdio.h>
+int main(){
+    int a = 8;
+    int b,fin=0;
+    for(int i = 1;i<=10;i++){
+        b = a*i;
+        fin +=b;
+    }
+    printf("sum of the numbers occurring in the multiplication table of 8 = %d",fin);
+    return 0;
+}
+
